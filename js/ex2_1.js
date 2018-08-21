aa59dedf91155994e9fab170f1dfb9f847dcf8d5
unknown <cristiano@cristianomartins.net>
2018-08-21 10:24:10 -0300

    execicio do livro

--- js/ex2_1.js
+++ js/ex2_1.js
@@ -1,9 +1,9 @@
 //declara a função mostrarOlá
 function mostrarOla() {
 //obtem o conteudo do campo (com id=) nome
-var nome = document.getElementById("nome").value;
+    var nome = document.getElementById("nome").value;
 //exibe no paragrafo (resposta): "Olá" e o nome informado
-document.getElementById("resposta").textContent = "Olá " + nome;
+    document.getElementById("resposta").textContent = "Olá " + nome;
 }
 //cria uma referencia
 var mostrar = document.getElementById("mostrar");
